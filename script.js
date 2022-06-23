e03cdba1b5a8e63190a84d92ec20610fca28e530
Shmuel Hoffman <snyh212@gmail.com>
2022-06-23 14:54:50 -0400

    transfer drive

---
+++ script.js
@@ -0,0 +1,20 @@
+/* * As a user, I want to play Rock, Paper, Scissors against an automated opponent.
+
+* As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
+
+* As a user, I expect the computer to choose R, P, or S in return.
+
+* As a user, I want the option to play again whether I win or lose.
+
+* As a user, I want to see my total wins, ties, and losses after each round.
+* Must use the `alert()`, `confirm()`, and `prompt()` methods to collect user input and display information to the user.
+
+* The computer's selection must be random to ensure a fair game. */
+
+var firstPrompt = window.prompt("Put in R, P, or S to play!")
+console.log(firstPrompt);
+
+var R > S,
+var R < P,
+var P < S;
+
